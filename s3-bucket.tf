0be09af5001624a6b8d920d81b667a00c33bf4db
Tom Su <tsu@infoblox.com>
2022-02-11 01:32:01 +0000

    Added s3 bucket module2

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,13 +1,7 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
+module "s3-bucket" {
+  source  = "app.terraform.io/tsu-lab/s3-bucket/aws"
+  version = "2.8.0"
+  # insert required variables here
   bucket_prefix = "tomsu"
-
-  versioning = {
-    enabled = true
-  }
-
 }
+
